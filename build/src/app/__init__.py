a0c06855ede2b068a36540587a3bf33972dd32b7
engelat <160321989+engelat@users.noreply.github.com>
2024-03-10 16:49:00 +0200

    Update __init__.py

--- build/src/app/__init__.py
+++ build/src/app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from app import views, apis  # Importing using absolute paths
+
 
 def create_app():
     app = Flask(__name__)
     with app.app_context():
-        # Views and APIs are now imported using absolute paths
-        pass
+        from . import views  # noqa: E402,F401
+        from . import apis  # noqa: E402,F401
     return app
